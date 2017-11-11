2532cb8c60cc75e025906bc60bbf1a8bfd24771b
hannes <jvarg@teknik.io>
2017-11-11 11:41:23 +0100

    missed the semicolon of doom

--- SimpleMutator.cpp
+++ SimpleMutator.cpp
@@ -6,7 +6,7 @@ SimpleMutator::SimpleMutator()
 {
 	/* For 8 bit lookup */
 	for (int i = 0; i<256; i++) {
-		POPCOUNT_LOT[i] = POPCOUNT_LOT[i / 2] + i & 1
+		POPCOUNT_LOT[i] = POPCOUNT_LOT[i / 2] + i & 1;
 	}
 
 }
