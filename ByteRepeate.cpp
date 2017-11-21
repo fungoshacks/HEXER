8d47532eeb14b365d837a2e7d0e2d00086486c97
hannes <jvarg@teknik.io>
2017-11-21 20:03:02 +0100

    changed occurences errir in ByteReplace, which prevents only replacings smaller than 10 characters. added byte repeate mutator, inserting in the middle of a file relies on making a copy of the first part, inserting the new content and then append the rest. [needs testing]

--- ByteRepeate.cpp
+++ ByteRepeate.cpp
@@ -1,12 +1,13 @@
-#include "ByteReplace.h"
+#include "ByteRepeate.h"
 
 
 Mutation *
-ByteReplace::mutate(string corpus)
+ByteRepeate::mutate(string corpus)
 {
 
 	Mutation *mutation;
 	FILE *f_mutation;
+	FILE *tempFile;
 	int rand_offset, f_size = 0;
 	unsigned char x = '\0';
 
@@ -18,8 +19,8 @@ ByteReplace::mutate(string corpus)
 	CopyFile(corpus.c_str(), mutation->getMutationPath().c_str(), false);
 	f_mutation = fopen(mutation->getMutationPath().c_str(), "wb+"); //open binary
 
-	int val[4] = { 0, 128, 196, 255};
-	int repeat[10] = { 1, 10, 32, 64, 128, 256, 512, 8, 16, 1024 };
+	unsigned int val[4] = { 0, 128, 196, 255};
+	unsigned int repeat[10] = { 1, 10, 32, 64, 128, 256, 512, 8, 16, 1024 };
 
 
 	if (f_mutation != NULL) {
@@ -35,17 +36,28 @@ ByteReplace::mutate(string corpus)
 			fseek(f_mutation, rand_offset, SEEK_SET);
 			fread(&x, 1, 1, f_mutation);
 			fseek(f_mutation, -1, SEEK_CUR);
-			x = val[rand() % 4]; //what byte?
-			unsigned int occurances = repeat[rand() % 10]; // how often do we append?
-			vector<char> buffer; //build char array which we want to write
+			char tmpX;
+			fread(&x, 1, 1, f_mutation);
+			unsigned int occurances = repeat[rand() % 10]; // how often do we clone?
+			vector<char> buffer; //build char array of clones which we want to write
 			for (unsigned int occ = 0; occ < occurances; occ++) {
 				buffer.push_back(x);
 			}
-			fwrite(buffer.data(), sizeof(char), sizeof(buffer.data()), f_mutation); //drop that shit
+			for (unsigned int dat = 0; dat < rand_offset; dat++) { //write first part of file to temp file
+				fread(&tmpX, 1, 1, f_mutation);
+				fwrite(&tmpX, 1, 1, tempFile);
+			}
+			
+			fwrite(buffer.data(), sizeof(char), sizeof(buffer.data()), tempFile); //drop that shit*n
+			
+			for (unsigned int dat = rand_offset; dat < f_size; dat++) { //write end of file to temp file
+				fread(&tmpX, 1, 1, f_mutation);
+				fwrite(&tmpX, 1, 1, tempFile);
+			}
 		}
+		f_mutation = tempFile;
 		fclose(f_mutation); //mic dropped.
 	}
 
 	return mutation;
 }
-
---
+++ ByteRepeate.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "Mutator.h"
+#include "Mutation.h"
+#include "Windows.h"
+#include <fstream>
+#include <stdlib.h> 
+#include <vector>
+
+class ByteRepeate :
+	public Mutator
+{
+public:
+	using Mutator::Mutator;
+	Mutation *mutate(string);
+};
+
