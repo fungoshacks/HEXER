ee4fecb35d152c8fd8b3250da648093cfdcd6463
hannes <jvarg@teknik.io>
2017-11-22 21:50:52 +0100

    Fixed ByteInvert

--- ByteInvert.cpp
+++ ByteInvert.cpp
@@ -1,6 +1,8 @@
 #include "ByteInvert.h"
+#include <iostream>
 
-unsigned char ByteInvert::reverse(unsigned char b) {
+unsigned char 
+ByteInvert::reverse(unsigned char b) {
 	b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
 	b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
 	b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
@@ -14,8 +16,9 @@ ByteInvert::mutate(string corpus)
 
 	Mutation *mutation;
 	FILE *f_mutation;
-	int rand_offset, f_size = 0;
-	unsigned char x = '\0';
+	int rand_offset, f_size;
+	int xor_values[8] = { 1,2,4,8,16,32,64,128 };
+	unsigned char x;
 
 	mutation = new Mutation();
 	mutation->setCorpus(corpus);
@@ -23,10 +26,7 @@ ByteInvert::mutate(string corpus)
 
 	/* Create a copy of corpus to mutate with */
 	CopyFile(corpus.c_str(), mutation->getMutationPath().c_str(), false);
-	f_mutation = fopen(mutation->getMutationPath().c_str(), "wb+"); //open binary
-
-	unsigned int repeat[10] = { 1, 10, 32, 64, 128, 256, 512, 8, 16, 1024 };
-
+	f_mutation = fopen(mutation->getMutationPath().c_str(), "r+");
 
 	if (f_mutation != NULL) {
 
@@ -38,19 +38,17 @@ ByteInvert::mutate(string corpus)
 
 			rewind(f_mutation);
 			rand_offset = rand() % f_size + 1;
-			unsigned char inverted;
-			unsigned int occurances = repeat[rand() % 10]; // how many bytes do we invert?
-			for (unsigned int occ = 0; occ < occurances; occ++) {
-				fseek(f_mutation, rand_offset+occ, SEEK_SET);
-				fread(&x, 1, 1, f_mutation);
-				fseek(f_mutation, -1, SEEK_CUR);
-				fread(&x, 1, 1, f_mutation);
-				inverted = reverse(x);
-				fwrite(&x, 1, 1, f_mutation);
-			}
-			
-			}
+			fseek(f_mutation, rand_offset, SEEK_SET);
+			fread(&x, 1, 1, f_mutation);
+			fseek(f_mutation, -1, SEEK_CUR);
+			x = reverse(x);
+			fwrite(&x, 1, 1, f_mutation);
+
 		}
-		fclose(f_mutation); //mic dropped.
-		return mutation;
+
+		fclose(f_mutation);
+
+	}
+
+	return mutation;
 }
