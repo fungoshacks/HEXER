c2b8621c478a836d4ced176f5676b895960d3c8d
hannes <jvarg@teknik.io>
2017-12-14 22:33:58 +0100

    added hexer ascii art ftw

--- main.cpp
+++ main.cpp
@@ -74,6 +74,52 @@ void
 logo()
 {
 
+    printf("                                    /sdN/                                                 \n");
+    printf("                                 :yNMmyyms/                                               \n");
+    printf("                               omMNhhyyyyhN.                                              \n");
+    printf("                             `ohyyydyyyyyyhh`                                             \n");
+    printf("                            oyyyhdmNyyyyyyymd`                                            \n");
+    printf("                           yMhsoyysmyyyyyyyyNm.                                           \n");
+    printf("                           ``     :NyyyyhyyyyNh                                           \n");
+    printf("                                  :MhyyyyyyyyhM:                                          \n");
+    printf("                                  :NyyyyyyyyyydN.                                         \n");
+    printf("                                  +dyyyyyyyyyyydy                                         \n");
+    printf("                                  yhyyyyyyhyyyyyN-                                        \n");
+    printf("                                  mdyyyyyyyyyyyyNh                                        \n");
+    printf("                                 :MyyyyyyyyyyyyyhM.                                       \n");
+    printf("                                 hmyyyyyyyyyyyyyyMs                                       \n");
+    printf("                                :Nyyyydmhyyyyyyyydd                                       \n");
+    printf("                                ddyyyddyNhyyyyyyyyNsooooo:                                \n");
+    printf("                      ``-/osyyshNyyyyN/.-hyyyyyyyydMdyyhdmmho:`                           \n");
+    printf("                  -shdddhhyyyyhNhyyyym-.:ddyyyyyyyyNNyhhyyyyyhmh+.                        \n");
+    printf("              .+hmhyyyyyyyyyyyhNyyyyydm/smyyyyyyyyymMhyyyyyyyyyyhmd/`                     \n");
+    printf("           `/dmhyyyyyyyyyyyyyyNhyyyyyyhMmyyyyyyyhNNmdyyyyyyyyyyyyyhdmd/.                  \n");
+    printf("          /dmhyyyyyyyhyyyyyyydNmmmdhhhyyyhhdmddhhhyyyyyyyyyyyyyyyyydhhNs.                 \n");
+    printf("            `:syyyNMNmdhhhyyyyyyyyhhhhhhhhhyyyyyyyyyyyyyyyyhhdmmNdh+-.                    \n");
+    printf("                   `:/oNmmNmdhhhhyyyyyyyyyhhhhddddmmmmmmNNNNyo+-`                         \n");
+    printf("                      .doy.:+oMMMdyhhyyhyyssso+///hNy--.hyso                              \n");
+    printf("                      /yym-mMhyddNm+:.........-sNhs++o/.m/+m-                             \n");
+    printf("                      oshN+NNooNMmhhm+.......omyoydhNM++h//+d`                            \n");
+    printf("                     `d+sNs.-/+shh/...../d/./s/dMMhsNN-ho///oh`                           \n");
+    printf("                     `y//dN-............-Nm-...:/+//+:-///////.                           \n");
+    printf("                     `///smh-........./y-:Nm-.........yo//////+h                          \n");
+    printf("                      .h/ysh/.........mm-.:Nm........-m//////oo:y                         \n");
+    printf("                     `hy/////:.....-:+MNNhysNo.......hs///////y/                          \n");
+    printf("                     `y/////////+s/ohyo+///////dy/-.+d/////o//d:                          \n");
+    printf("                    `ho////h+///hs/+yhhdddmddmhyoyh+///////ds/sy`                         \n");
+    printf("                    ys/od//m///od/sMh/--.:-  `/mN+/s+/////odo//mo                         \n");
+    printf("                   -y///m//d+//m+/sm/+++++++++yhmo/soho///oo///hd`                        \n");
+    printf("                   s--d+y/////s///sssssoosoooooo/////yh////////y:                         \n");
+    printf("                      +h//////sd//////////ds//////////ds//////+d`                         \n");
+    printf("                     s:moso////yd////////oh/////o////+m+//+hhsh+                          \n");
+    printf("                     `yho:hs////sd+//+///++/////m////yy//sho-//                           \n");
+    printf("                          `ys////yy//m+//do/////do///o//hh+:.                             \n");
+    printf("                            ys///+m//ho//sh/////do////o/::.                               \n");
+    printf("                             y////o//o+//+h//+//s///+s.                                   \n");
+    printf("                               yy+/////////////y///o+.                                    \n");
+    printf("                                :dsyho//////+++hoos.                                      \n");
+    printf("                                  ` `/+/yso/+:/`                                          \n");
+
     printf("\t\t\t _   _  _______   __ ___________\n");
     printf("\t\t\t| | | ||  ___\\ \\ / /|  ___| ___ \n");
     printf("\t\t\t| |_| || |__  \\ V / | |__ | |_/ /\n");
